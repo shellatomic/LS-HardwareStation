8a5e80dc0704a3b2ae2655b390625051538294d9
shellatomic <shellatomic@hotmail.com>
2023-01-07 16:00:52 +0300

    Second commit

--- posLogoService.js
+++ posLogoService.js
@@ -1,16 +1,11 @@
-var Service = require('node-windows').Service;
+var Service = require("node-windows").Service;
 
 var svc = new Service({
-  name:'POS Logo & Barcode & QR Helper',
-  description: 'LS BUSINESS CENTRAL POS Logo & Barcode & QR Helper',
-  script: 'D:\\posLogo\\route.js',
-  nodeOptions: [
-    '--harmony',
-    '--max_old_space_size=4096'
-  ],
+  name: "LS Hardware stationV1",
+  description: "LS Hardware station for receipt Logo & Barcode & QRCode",
+  script: "D:\\posLogo\\route.js",
+  nodeOptions: ["--harmony", "--max_old_space_size=4096"],
 });
-
-
 
 // svc.on('install',function(){
 //   svc.start();
--- route.js
+++ route.js
@@ -1,39 +1,62 @@
-const express = require('express');
+const express = require("express");
 var crypto = require("crypto");
-var fs = require("fs");
 const path = require("path");
-
 const multer = require("multer");
 
 const app = express();
 
-const port = 8094
+const port = 8094;
 
+// const fileMemoryStorage = multer.memoryStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "C:/temp");
+//   },
+//   filename: (req, file, cb) => {
+//     var id = crypto.randomBytes(20).toString("hex");
+//     cb(null, id + path.extname(file.originalname));
+//   },
+// });
 
+const fileDiskStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "C:/temp");
+  },
+  filename: (req, file, cb) => {
+    var id = crypto.randomBytes(20).toString("hex");
+    cb(null, id + path.extname(file.originalname));
+  },
+});
 
-const imageStorage = multer.diskStorage({
-    destination: "C:/temp", // Destination to store image 
-    filename: (req, file, cb) => {
- 	var id = crypto.randomBytes(20).toString('hex');
-        cb(null, id + path.extname(file.originalname))
+const imagesUpload = multer({
+  storage: fileDiskStorage,
+  //   limits: {
+  //     fileSize: 1000000, // 1000000 Bytes = 1 MB
+  //   },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|bmp)$/)) {
+      return cb(new Error("Please upload an image"));
     }
+    cb(undefined, true);
+  },
 });
 
-const imageUpload = multer({
-    storage: imageStorage,
-    limits: {
-        fileSize: 1000000   // 1000000 Bytes = 1 MB
-    },
-   
-})  
+app.post("/api/invoice/upload", imagesUpload.single("file"), (req, res) => {
+  console.log(req.file);
 
-
-
-app.post('/api/invoice/upload', 
- 	imageUpload.single("file"),
-	(req, res) => {
-
-    res.json(`${req.file.destination}/${req.file.filename}`);
+  res.json(`${req.file.destination}/${req.file.filename}`);
 });
 
-app.listen(port, () => console.log(`app listening on port ${port}`))
+app.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    if (err.code === "LIMIT_FILE_SIZE") {
+      res.status(413).json("File too large");
+    }
+    if (err.code === "LIMIT_UNEXPECTED_FILE") {
+      res.status(415).json("Too many files");
+    }
+  } else {
+    res.status(500).json(err.message);
+  }
+});
+
+app.listen(port, () => console.log(`app listening on port ${port}`));
