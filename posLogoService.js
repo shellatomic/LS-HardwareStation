9cecf0c3d2c9a2f356bb918a4e45569bfcff916b
shellatomic <74080649+shellatomic@users.noreply.github.com>
2023-01-08 10:16:02 +0300

    Changing path in posLogoService

--- posLogoService.js
+++ posLogoService.js
@@ -3,11 +3,12 @@ var Service = require("node-windows").Service;
 var svc = new Service({
   name: "LS Hardware stationV1",
   description: "LS Hardware station for receipt Logo & Barcode & QRCode",
-  script: "D:\\posLogo\\route.js",
+  script:
+    "C:\\Users\\aaabudayeh\\Desktop\\LS-HardwareStation\\posLogoService.js",
   nodeOptions: ["--harmony", "--max_old_space_size=4096"],
 });
 
-// svc.on('install',function(){
+// svc.on("install", function () {
 //   svc.start();
 // });
 
